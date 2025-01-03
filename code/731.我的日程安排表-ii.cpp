b542769bf53ea0030b8b87c14f0c79e13efbef73
SWXCN <1149586912@qq.com>
2025-01-03 19:28:18 +0800

    731

---
+++ code/731.我的日程安排表-ii.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=731 lang=cpp
+ *
+ * [731] 我的日程安排表 II
+ */
+
+// @lc code=start
+class MyCalendarTwo {
+public:
+    MyCalendarTwo() {
+
+    }
+
+    void update(int start, int end, int val, int l, int r, int idx) {
+        if (r < start || end < l) {
+            return;
+        } 
+        if (start <= l && r <= end) {
+            tree[idx].first += val;
+            tree[idx].second += val;
+        } else {
+            int mid = (l + r) >> 1;
+            update(start, end, val, l, mid, 2 * idx);
+            update(start, end, val, mid + 1, r, 2 * idx + 1);
+            tree[idx].first = tree[idx].second + max(tree[2 * idx].first, tree[2 * idx + 1].first);
+        }
+    }
+
+    bool book(int start, int end) {            
+        update(start, end - 1, 1, 0, 1e9, 1);
+        if (tree[1].first > 2) {
+            update(start, end - 1, -1, 0, 1e9, 1);
+            return false;
+        }
+        return true;
+    }
+private:
+    unordered_map<int, pair<int, int>> tree;
+};
+
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
+// @lc code=end
+
